b7ccdb6b5e5558a4a9d0d4f0050e884865e005ab
Brittan Robinett <brittan.robinett@comcast.net>
2019-09-11 22:38:20 -0700

    installed bcrypt, created registration routes and pw encryption

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
     firstName: { type: String, required: true, trim: true },
     lastName: { type: String, required: true, trim: true },
     email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true },
     groups: [{ type: Schema.Types.ObjectId, ref: 'Group' }]
 }, {
         timestamps: true,
--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 let Group = require('../models/group.model');
 
+// pre: There are groups
+// post: Return array of all group objects
 router.route('/').get((req, res) => {
     Group.find()
         .then(groups => res.json(groups))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// pre: groupName on group creation page
+// post: Create new group object using name inputted by user
 router.route('/add').post((req, res) => {
     const groupName = req.body.groupName;
 
